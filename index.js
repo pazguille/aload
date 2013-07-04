ffe652738a5f11da67fa09fc51a1f08462a7dd07
Guille Paz <guille87paz@gmail.com>
2013-07-04 08:47:21 -0300

    Create asyncall v0.0.2.

---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+ * Loads images, scripts, styles, iframes, videos and audios asynchronously.
+ * @param {NodeList} [nodes] - A NodeList of elements. By default, its value is the result of `querySelectorAll('[data-async]'
+ * @returns {NodeList}
+ */
+module = module.exports = function (nodes) {
+
+    nodes = nodes || window.document.querySelectorAll('[data-async]');
+
+    var i = 0,
+        len = nodes.length,
+        src,
+        node;
+
+    for (i; i < len; i += 1) {
+        node = nodes[i];
+        src = (node.tagName) !== 'LINK' ? 'src' : 'href';
+        node[src] = node.getAttribute('data-async');
+        node.removeAttribute('data-async');
+    }
+
+    return nodes;
+};
