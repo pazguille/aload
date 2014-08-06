5fc47908e9a3342cdd9756935681dc1fa87751b8
Guille Paz <guille87paz@gmail.com>
2014-08-06 07:57:12 -0300

    Remove UMD support.

--- aload.js
+++ aload.js
@@ -1,50 +1,26 @@
-(function (window) {
-    'use strict';
+/**
+ * Loads images, scripts, styles, iframes, videos and audios asynchronously.
+ * @param {NodeList} [nodes] - A NodeList of elements. By default, it is the result of `querySelectorAll('[data-aload]')`.
+ * @returns {NodeList}
+ */
+function aload(nodes) {
+  'use strict';
 
-    /**
-     * Loads images, scripts, styles, iframes, videos and audios asynchronously.
-     * @param {NodeList} [nodes] - A NodeList of elements. By default, it is the result of `querySelectorAll('[data-aload]')`.
-     * @returns {NodeList}
-     */
-    function aload(nodes) {
+  nodes = nodes || window.document.querySelectorAll('[data-aload]');
 
-        nodes = nodes || window.document.querySelectorAll('[data-aload]');
+  if (nodes.length === undefined) {
+    nodes = [nodes];
+  }
 
-        if (nodes.length === undefined) {
-            nodes = [nodes];
-        }
+  var i = 0,
+      len = nodes.length,
+      node;
 
-        var i = 0,
-            len = nodes.length,
-            src,
-            node;
+  for (i; i < len; i += 1) {
+    node = nodes[i];
+    node[ node.tagName !== 'LINK' ? 'src' : 'href' ] = node.getAttribute('data-aload');
+    node.removeAttribute('data-aload');
+  }
 
-        for (i; i < len; i += 1) {
-            node = nodes[i];
-            src = (node.tagName) !== 'LINK' ? 'src' : 'href';
-            node[src] = node.getAttribute('data-aload');
-            node.removeAttribute('data-aload');
-        }
-
-        return nodes;
-    }
-
-    /**
-     * Expose aload
-     */
-    // AMD suppport
-    if (typeof window.define === 'function' && window.define.amd !== undefined) {
-        window.define('aload', [], function () {
-            return aload;
-        });
-
-    // CommonJS suppport
-    } else if (typeof module !== 'undefined' && module.exports !== undefined) {
-        module.exports = aload;
-
-    // Default
-    } else {
-        window.aload = aload;
-    }
-
-}(this));
+  return nodes;
+}
